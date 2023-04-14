97036d86661b62c89c72fcd9505c77ae2da22d81
Еlena Anisimova <elena.anis.12@yandex.ru>
2023-04-14 21:19:57 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let res = [];
+  let idDeleted;
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case `--discard-next`:
+        idDeleted = i + 1;
+        i++;
+        break;
+      case `--discard-prev`:
+        if (idDeleted != i - 1) {
+          idDeleted = i;
+          res.pop();
+        }
+        break;
+      case `--double-next`:
+        if (arr[i + 1] !== undefined) {
+          res.push(arr[i + 1], arr[i + 1]);
+          i++;
+        }
+        break;
+      case `--double-prev`:
+        if (arr[i - 1] !== undefined && idDeleted != i - 1) {
+          res.push(arr[i - 1]);
+        }
+        break;
+      default: res.push(arr[i]);
+    }
+  }
+  return res;
 }
 
 module.exports = {
   transform
 };
+transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]);
